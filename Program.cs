6925d46040be86cc70dd0a823555fa76247c4e05
Mehul Sankhe <Mehul Sankhe@LAPTOP-2K3FS8U>
2026-06-22 16:15:11 +0100

    Function implementation to perform left rotation of a static hardcoded array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Function to perform left circular rotation of an array");
+
+int[] myArray = new int[5] {23,56,76,43,12};
+Console.WriteLine("Orignal Array:");
+foreach(int i in myArray)
+{
+    Console.Write(i+" ");
+}
+int[] newArray = new int[myArray.Length];
+for(int i=0; i<myArray.Length; i++)
+{
+    newArray[i] = myArray[(i + 1) % myArray.Length];
+}
+Console.WriteLine("After Left rotation of array the sequence is: ");
+foreach(int j in newArray)
+{
+    Console.Write(j+" ");
+}
