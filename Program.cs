118c85d6b01f53ff445dd29a51fffbe45600c035
Mehul Sankhe <Mehul Sankhe@LAPTOP-2K3FS8U>
2026-06-22 17:06:06 +0100

    Made changes to the function to take dynamic input from user for array elements.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Function to perform left circular rotation of an array");
 
-int[] myArray = new int[5] {23,56,76,43,12};
-Console.WriteLine("Orignal Array:");
-foreach(int i in myArray)
+try
 {
-    Console.Write(i+" ");
+    Console.WriteLine("Enter 5 array elements with spaces in between them");
+    string arrayelements = Console.ReadLine();
+    int[] myArray = arrayelements.Split(" ").Select(int.Parse).ToArray();
+    Console.WriteLine("Orignal Array:");
+    foreach (int i in myArray)
+    {
+        Console.Write(i + " ");
+    }
+    int[] newArray = new int[myArray.Length];
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        newArray[i] = myArray[(i + 1) % myArray.Length];
+    }
+    Console.WriteLine("\nAfter Left rotation of array the sequence is: ");
+    foreach (int j in newArray)
+    {
+        Console.Write(j + " ");
+    }
 }
-int[] newArray = new int[myArray.Length];
-for(int i=0; i<myArray.Length; i++)
+catch
 {
-    newArray[i] = myArray[(i + 1) % myArray.Length];
-}
-Console.WriteLine("After Left rotation of array the sequence is: ");
-foreach(int j in newArray)
-{
-    Console.Write(j+" ");
+    Console.WriteLine("\nSomething went wrong, check if you inputted array elements with proper spaces by eleminating any front or tailing spaces");
 }
